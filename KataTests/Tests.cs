94d5e651ce68881efc58c353f37ea94bc06f3119
dickyp <richard.peckett@outlook.com>
2020-11-20 13:00:08 +0000

    added array left right side equality check

--- KataTests/Tests.cs
+++ KataTests/Tests.cs
@@ -1,7 +1,7 @@
 using cSharpKata.Katas;
 using Xunit;
 
-namespace KataTests.Tests
+namespace KataTests
 {
     public class Tests
     {
@@ -24,6 +24,15 @@ namespace KataTests.Tests
             Assert.Equal(2, CharacterCounting.DuplicateCount("aabBcde"));
             Assert.Equal(1, CharacterCounting.DuplicateCount("Indivisibility"));
             Assert.Equal(2, CharacterCounting.DuplicateCount("Indivisibilities"));
+        }
+
+        [Fact]
+        public void EqualSidesArrayTest()
+        {
+            Assert.Equal(3, EqualSidesArray.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+            Assert.Equal(1, EqualSidesArray.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+            Assert.Equal(-1,EqualSidesArray.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.Equal(3, EqualSidesArray.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
         }
     }
 }
---
+++ cSharpKata/Katas/EqualSidesArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace cSharpKata.Katas
+{
+    public class EqualSidesArray
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var leftTake = arr.Take(i);
+                var leftTotal = leftTake.Sum();
+
+                var rightTake = arr.Skip(i + 1);
+                var rightTotal = rightTake.Sum();
+
+                if (leftTotal == rightTotal)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
