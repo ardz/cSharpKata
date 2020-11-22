7d1d243fb3e8172dde32d9cf076786070f42e82b
dickyp <richard.peckett@outlook.com>
2020-11-22 18:22:16 +0000

    better

--- cSharpKata/Katas/ArrayDiff.cs
+++ cSharpKata/Katas/ArrayDiff.cs
@@ -7,9 +7,7 @@ namespace cSharpKata.Katas
     {
         public static int[] CalculateDiff(int[] a, int[] b)
         {
-            HashSet<int> hash = new HashSet<int>(b);
-
-           return a.Where(_ => !hash.Contains(_)).ToArray();
+            return a.Where(_ => !b.Contains(_)).ToArray();
         }
     }
 }
