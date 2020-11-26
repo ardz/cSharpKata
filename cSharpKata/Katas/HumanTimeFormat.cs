e51de5394111ee7759c41d4d148f71f9bef4da7a
dickyp <richard.peckett@outlook.com>
2020-11-26 21:55:57 +0000

    code cleanup

--- cSharpKata/Katas/HumanTimeFormat.cs
+++ cSharpKata/Katas/HumanTimeFormat.cs
@@ -13,41 +13,44 @@ namespace cSharpKata.Katas
                 return "now";
             }
 
-            var foo = new Dictionary<string, int>();
-
             var duration = TimeSpan.FromSeconds(inputSeconds);
+            var allTimeDurations = new Dictionary<string, int>();
 
             // work out how many whole years from seconds we're dealing with
             var years = duration.Days / 365;
             var yearsLabel = years > 1 ? "years" : "year";
-            foo.Add(yearsLabel, years);
+            allTimeDurations.Add(yearsLabel, years);
 
             // work out what the days remaining are
             var wholeYearsInDays = years * 365;
             var days = duration.Days - wholeYearsInDays;
             var daysLabel = days > 1 ? "days" : "day";
-            foo.Add(daysLabel, days);
+            allTimeDurations.Add(daysLabel, days);
 
+            // add the rest
             var hours = duration.Hours;
             var hoursLabel = duration.Hours > 1 ? "hours" : "hour";
-            foo.Add(hoursLabel, hours);
+            allTimeDurations.Add(hoursLabel, hours);
 
             var minutes = duration.Minutes;
             var minutesLabel = duration.Minutes > 1 ? "minutes" : "minute";
-            foo.Add(minutesLabel, minutes);
+            allTimeDurations.Add(minutesLabel, minutes);
 
             var seconds = duration.Seconds;
             var secondsLabel = duration.Seconds > 1 ? "seconds" : "second";
             var readableSeconds = seconds + secondsLabel;
-            foo.Add(secondsLabel, seconds);
+            allTimeDurations.Add(secondsLabel, seconds);
 
-            var nonZeroValues = foo.Where(x => x.Value > 0).Select(x => x.Value + " " + x.Key);
+            // only get the time values where value isn't 0
+            var nonZeroValues = allTimeDurations.Where(x => x.Value > 0).Select(x => x.Value + " " + x.Key);
 
+            // edge case, if there's only one in the list, don't output the "and" just output the time value
             if (nonZeroValues.Count() == 1)
             {
                 return nonZeroValues.First();
             }
 
+            // else just return the string joined up with commas and "and"
             return string.Join(", ", nonZeroValues.Take(nonZeroValues.Count() - 1)) + " and " + nonZeroValues.Last();
         }
     }
